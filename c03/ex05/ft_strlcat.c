5282159cc8f05ff7eb91bdb10b82d7f9a46b48d4
WarfaXDVR <152220767+Warfa777@users.noreply.github.com>
2023-12-11 02:00:19 +0100

    Add files via upload

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliberat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 17:50:16 by sliberat          #+#    #+#             */
+/*   Updated: 2023/12/10 22:21:14 by sliberat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && i < size - 1)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (i);
+}
